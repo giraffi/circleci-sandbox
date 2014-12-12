9fbcbb613f7d98e5b9a49c14117c2e938dcd06de
sawanoboly <sawanoboriyu@higanworks.com>
2014-12-12 15:07:25 +0900

    set HOME and cache directory

--- ci/01_eb_command.sh
+++ ci/01_eb_command.sh
@@ -3,15 +3,17 @@ set -ex
 
 EB_VERSION="2.6.4"
 EB_BASE="AWS-ElasticBeanstalk-CLI-${EB_VERSION}"
+HOME=${HOME:-/home/ubuntu}
 
 sudo pip install boto
 
-cd /home/ubuntu
+mkdir -p ${HOME}/awstools
+cd $HOME/awstools
 
-if ! [ -d /home/ubuntu/${EB_BASE} ]; then
+if ! [ -d ${HOME}/awstools/${EB_BASE} ]; then
   wget https://s3.amazonaws.com/elasticbeanstalk/cli/${EB_BASE}.zip
   unzip ${EB_BASE}.zip
 fi
 
-sudo ln -fs /home/ubuntu/${EB_BASE}/eb/linux/python2.7/eb /home/ubuntu/bin/
-sudo ln -fs /home/ubuntu/${EB_BASE}/AWSDevTools/Linux/AWSDevTools-RepositorySetup.sh /home/ubuntu/bin/
+sudo ln -fs ${HOME}/awstools/${EB_BASE}/eb/linux/python2.7/eb ${HOME}/bin/
+sudo ln -fs ${HOME}/awstools/${EB_BASE}/AWSDevTools/Linux/AWSDevTools-RepositorySetup.sh ${HOME}/bin/
