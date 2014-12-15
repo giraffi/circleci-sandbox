7b88f1773c0ff865b99a730b16c460112dc57e4a
sawanoboly <sawanoboriyu@higanworks.com>
2014-12-15 13:31:19 +0900

    Revert "set eb credencals by chef"
    This reverts commit 05d8b2a9e2e0ecaede111dd0c416dade75d21e1e.

--- ci/recipe_set_eb_credentials.rb
+++
@@ -1,8 +0,0 @@
-template File.join(ENV['HOME'], '.aws', 'eb_credentials') do
-  source File.join(ENV['HOME'], ENV['CIRCLE_PROJECT_REPONAME'], 'ci/templates/eb_credentials.erb')
-  owner 'ubuntu'
-  group 'ubuntu'
-  mode '0600'
-  local true
-end
-
