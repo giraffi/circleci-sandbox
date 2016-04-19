2795e8e561ad0b8fa297a5f7330c5c20dab0dea9
sawanoboly <sawanoboriyu@higanworks.com>
2016-04-19 17:40:01 +0900

    fetch tags

--- ci/99_sync_bitbucket.sh
+++ ci/99_sync_bitbucket.sh
@@ -8,8 +8,9 @@ IdentityFile /home/ubuntu/.ssh/id_circleci_github
 EOL
 
 git remote add bitbucket `git remote get-url origin | sed 's/github.com/bitbucket.org/'`
-git remote prune origin 
-git push --mirror --prune
+git remote prune origin
+git fetch origin --tags
+git push bitbucket --mirror --prune
 git push bitbucket --tags
 git remote prune bitbucket
 
